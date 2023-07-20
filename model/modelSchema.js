8bb7d68f5471d9fda6405cd3d9f226507208d447
rajchatterz <iamraj.space@gmail.com>
2023-07-20 21:16:55 +0530

    Done with the schema modelling

---
+++ model/modelSchema.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const productSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:[true,'Please ENter a product name']
+    },
+    quantity:{
+        type:Number,
+        required:true,
+        default:0
+    },
+    price:{
+        type:Number,
+        required:true
+    },
+    image:{
+        type:String,
+        required:false
+    }
+},{ 
+    timestamps:true
+})
+
+const Product = mongoose.model("Product",productSchema)
+module.exports=Product
