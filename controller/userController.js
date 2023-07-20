e3be67f374415f7b27f6cdb324e25ff05a3f542c
rajchatterz <iamraj.space@gmail.com>
2023-07-20 23:23:37 +0530

    having some issues with update function in crud

--- controller/userController.js
+++ controller/userController.js
@@ -1,3 +1,4 @@
+
 const productSchema = require('../model/modelSchema')
 
 const create = async(req,res,next)=>{
@@ -29,7 +30,29 @@ const read = async(req,res,next)=>{
         })
     }
 }
+const update =async (req,res,next)=>{
+    try {
+        const {id} =req.params
+        const updateProduct = await productSchema.findByIdAndUpdate(id,req.body)
+        if(!updateProduct){
+            return res.status(400).json({
+                success:false,
+                message:"we cant find the product"
+            })
+        }
+        const recentUpdate = await productSchema.findById(id)
+        res.status(200).json({
+            success:true,
+            message:updateProduct
+        })
+    } catch (error) {
+        res.status(400).json({
+
+        })
+    }
+}
 module.exports={
     create,
-    read
+    read,
+    update
 }
--- route/userRoute.js
+++ route/userRoute.js
@@ -1,8 +1,9 @@
 const express = require('express')
-const {create,read} = require('../controller/userController')
+const {create,read,update} = require('../controller/userController')
 
 const userRouter = express.Router()
 
 userRouter.post('/create',create)
 userRouter.get('/read',read)
+userRouter.put('/update',update)
 module.exports=userRouter
